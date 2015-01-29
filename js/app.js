23d82992da923d2a12061dda95f17c7d07db10c5
Nicole Cano <arbolkiri@gmail.com>
2015-01-28 18:28:13 -0600

    setup

--- js/app.js
+++ js/app.js
@@ -10,11 +10,15 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/lodash/dist/lodash.min.js"},
+        {url: "./bower_components/backbone/backbone.js"},
+        {url: "./js/github.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
+        var token = "dfb1cfcd29b2b0bcf67348aff559c4a96ea7dddd";
+        var right_template_url= "./templates/rightside.html";
+        new GithubClient(token);
     })
 
 }
